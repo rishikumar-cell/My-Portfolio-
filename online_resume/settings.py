df03df669a1815a091f780453aac34fa7566dbf4
Venu Kumar Chittimalla <132468453+rishikumar-cell@users.noreply.github.com>
2025-04-28 11:30:31 +0530

    Update settings.py

--- online_resume/settings.py
+++ online_resume/settings.py
@@ -13,6 +13,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
+
+
+PORT = os.environ.get('PORT', 8000)  # Default to 8000 locally
+
+# Example if using gunicorn (recommended):
+# In Railway dashboard, set "Start Command" as:
+gunicorn online_resume.wsgi --bind 0.0.0.0:$PORT
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
