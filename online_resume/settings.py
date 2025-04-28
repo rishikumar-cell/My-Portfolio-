07837c70b72c8ede98482305c926228b89994340
Venu Kumar Chittimalla <132468453+rishikumar-cell@users.noreply.github.com>
2025-04-28 10:15:31 +0530

    Update settings.py

--- online_resume/settings.py
+++ online_resume/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
